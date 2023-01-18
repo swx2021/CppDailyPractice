ffa9702ff23529e746939ffe61979f2ddf0d9b02
it-writer <16593550@qq.com>
2023-01-18 21:02:16 +0800

    20230118

--- 20230118/text.cpp
+++ 20230118/text.cpp
@@ -48,32 +48,110 @@
 //}
 
 //题目链接：https://www.nowcoder.com/practice/0787459e92bc4360ba48b094b7f21577?tpId=225&tqId=2368656&ru=/exam/oj&qru=/ta/primary-grammar-cpp/question-ranking&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26tab%3D%25E8%25AF%25AD%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D225
-//#include<bits/stdc++.h>
-#include<iostream>
-#include<map>
+////#include<bits/stdc++.h>
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//int main() {
+//	//write your code here......
+//	int n, m,k, x;
+//	cin >> n >> m;
+//	map<int, bool>mp;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> k;
+//		mp[k] = true;
+//		//pair<int,bool> p = make_pair(x, true);
+//	}
+//	for (int i = 0; i < m; i++)
+//	{
+//		cin >> x;
+//		if (mp[x])
+//			cout << "yes" << endl;
+//		else
+//			cout << "no" << endl;
+//	}
+//	
+//
+//
+//
+//	return 0;
+//}
+
+//题目链接：https://www.nowcoder.com/practice/0f02d35dcd564f0a87865d604eccbe18?tpId=225&tqId=2190741&ru=/exam/oj&qru=/ta/primary-grammar-cpp/question-ranking&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26tab%3D%25E8%25AF%25AD%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D225
+#include <iostream>
 using namespace std;
+
+class Cube {
+
+    // write your code here......
+    int _length;
+    int _width;
+    int _height;
+public:
+    /*Cube(int length, int width, int height)
+        :_length(length),
+        _width(width),
+        _height(height)*/
+    void setLength(int length)
+    {
+        _length = length;
+    }
+
+    void setWidth(int width)
+    {
+        _width = width;
+    }
+
+    void setHeight(int height)
+    {
+        _height = height;
+    }
+
+    int getLength()
+    {
+        return _length;
+    }
+
+    int getWidth()
+    {
+        return _width;
+    }
+
+    int getHeight()
+    {
+        return _height;
+    }
+
+    int getArea() 
+    {
+        return (_length * _width + _length * _height + _width * _height)*2;
+    }
+
+    int getVolume()
+    {
+        return _length * _width * _height;
+    }
+
+};
+
 int main() {
-	//write your code here......
-	int n, m,k, x;
-	cin >> n >> m;
-	map<int, bool>mp;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> k;
-		mp[k] = true;
-		//pair<int,bool> p = make_pair(x, true);
-	}
-	for (int i = 0; i < m; i++)
-	{
-		cin >> x;
-		if (mp[x])
-			cout << "yes" << endl;
-		else
-			cout << "no" << endl;
-	}
-	
 
+    int length, width, height;
+    cin >> length;
+    cin >> width;
+    cin >> height;
 
+    Cube c;
+    c.setLength(length);
+    c.setWidth(width);
+    c.setHeight(height);
 
-	return 0;
+    cout << c.getLength() << " "
+        << c.getWidth() << " "
+        << c.getHeight() << " "
+        << c.getArea() << " "
+        << c.getVolume() << endl;
+
+    return 0;
 }
